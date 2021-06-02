eccba70a9bc29c6b4cb1d3f81b61dfbb67c4fbdf
PhantomKnight <gillgurpal699@gmail.com>
2021-06-02 14:52:34 +0530

    Added battery percentage thing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Getting infos about the visitor
-const memory = navigator.memory
+const memory = navigator.deviceMemory
 const touchPoints = navigator.maxTouchPoints
 const platfrom = navigator.platform
 const cores = navigator.hardwareConcurrency
@@ -19,7 +19,7 @@ visits=localStorage.getItem('visits')
 //Setting the phrases
 let coresPhrase=`Your device has ${cores} cores.`
 let visitPhrase = `You have visited this website ${visits} time(s).`
-let memoryPhrase= `Your device has ${memory} ram.`
+let memoryPhrase= `Your device has ${memory} GB ram.`
 let touchPhrase
 if (touchPoints>1){ touchPhrase = `You device has touch screen support.`}
 else { touchPhrase=`Your device does not support touch screen.`}
@@ -42,28 +42,77 @@ function apply(){
 }
 function fancy(){
     setInterval(()=>{
-        document.querySelector('.head').textContent='Your device information.'
+        document.querySelector('.head').innerHTML='<b>Your device information...</b>'
+        
         
         apply()
-        fetch()
-    },3)
-    document.querySelector('.head').textContent='Loading...'
+    },3000)
+    document.querySelector('.head').innerHTML='<b>Loading...</b>'
+    document.querySelector('.body-head').innerHTML='<b>Loading....</b>'
     
 }
 fancy()
-async function fetch(){
-    let xhr = new XMLHttpRequest
-    xhr.open('GET','https://ip-alternative.herokuapp.com/',true)
-    xhr.onload=function (){
-        let a = JSON.parse(this.responseText)
-        document.querySelector('.ip').textContent=`Your public ip address is ${a.ip}.`
-        document.querySelector('.isp').textContent=`Your Internet Service Provider is ${a.isp}.`
-        document.querySelector('.city').textContent=`Your city is ${a.city}.`
-        document.querySelector('.continent').textContent=`Your continent is  ${a.continent}.`
-        document.querySelector('.pin').textContent=`Your pin is around ${a.pin}.`
-        document.querySelector('.country').textContent=`Your country is ${a.country}.`
-        document.querySelector('.header').textContent=`Your browser's header is ${a.headers}.`
-        document.querySelector('.timezone').textContent=`Your timezone is ${a.timezone}`
+async function dat(){
+    let response = await fetch('https://ip-alternative.herokuapp.com',
+    {headers:{'Content-type':'application/json'},
+method:'GET'})
+    return response.json()
+           
+}
+let items={}
+
+dat().then(response=>{
+    items={headers:response.headers,ip:response.ip,isp:response.isp,city:response.city,pin:response.pin,timeZone:response.timezone,continent:response.continent,country:response.country}
+    document.querySelector('.header').innerHTML=`<b>${items.headers}</b>`
+    document.querySelector('.ip').textContent=`Your public ip is ${items.ip}`
+    document.querySelector('.isp').textContent=`Your internet service provider is  ${items.isp}`
+    document.querySelector('.city').textContent=`Your city is ${items.city}`
+    document.querySelector('.pin').textContent=`Your area pincode is around ${items.pin}`
+    document.querySelector('.timeZone').textContent=`Your time zone is ${items.timeZone}`
+    document.querySelector('.country').textContent=`Your continent is ${items.country}`
+    document.querySelector('.continent').textContent=`Your continent is ${items.continent}`
+    async function weather(){
+        let data = await  fetch(`https://ip-alternative.herokuapp.com/weather/${items.city}`,
+        {headers:{'Content-type':'application/json'},
+method:'GET'})
+        return data.json()
     }
-    xhr.send()
+weather().then(response=>{
+    console.log(response)
+    document.querySelector('.temperature').textContent=`The temperature of your city is ${response.temperature}℃`
+    document.querySelector('.weather').textContent=`The weather of your city is ${response.description}`
+    document.querySelector('.body-head').innerHTML='<b>Your location and weather information...<b>'
+
+})
+
+})
+//Checking for battery percentage support
+let batterySupport = 'getBattery' in navigator
+if (batterySupport===true){
+    console.log('The battery is supported')
+    let batteryPromise = navigator.getBattery();
+    batteryPromise.then(batteryCallback);
+
+    function batteryCallback(batteryObject) {
+    printBatteryStatus(batteryObject);
+    }
+    function printBatteryStatus(batteryObject) {
+        console.log("IsCharging", batteryObject.charging);
+        console.log("Percentage", batteryObject.level);
+        setInterval(()=>{
+            document.querySelector('.battery-head').innerHTML='<b>Your battery information...</b>'
+            if (batteryObject.charging===true){
+            document.querySelector('.charging-status').textContent=`Your device is charging.`}
+            else{document.querySelector('.charging-status').textContent=`Your device is not charging.`}
+            document.querySelector('.battery-percentage').textContent=`Your battery percentage is nearly about ${batteryObject.level}`
+        },3000)
+        document.querySelector('.battery-head').innerHTML='<b>Loading...</b>'
+
+    
+        console.log("charging Time", batteryObject.chargingTime);
+        console.log("DisCharging Time", batteryObject.dischargingTime);
+}
+}
+else{
+    console.log('battery is not supported')
 }
